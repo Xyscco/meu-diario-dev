3522e4ced22e4242169ebb37981435629676f45a
Francisco <marciocmiranda2504@gmail.com>
2026-02-18 11:24:59 -0300

    feat: Remover arquivos de configuração do ambiente para Supabase

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  supabase: {
-    url: 'YOUR_SUPABASE_URL',
-    publishableKey: 'YOUR_SUPABASE_PUBLISHABLE_KEY'
-  }
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  supabase: {
-    url: 'https://fylwawsfjpvpvlxxfkdc.supabase.co',
-    publishableKey: 'sb_publishable_O0P6X2NsVQBXQiCGwdEc9Q_1udPVDhn'
-  }
-};
